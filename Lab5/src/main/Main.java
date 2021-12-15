7fb099aa1028d028645ce1cff534d4715a4cfa8c
CostinOnciu <67917784+CostinOnciu@users.noreply.github.com>
2021-12-15 08:28:58 +0200

    lab8

--- Lab5/src/main/Main.java
+++ Lab5/src/main/Main.java
@@ -1,16 +1,33 @@
 package main;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    public static List<Symbol> ParseInputFile(String filename) throws FileNotFoundException {
+        List<Symbol> symbols = new ArrayList<>();
+        Scanner fileInput = new Scanner(new File(filename));
+        while(fileInput.hasNext())
+        {
+            String line = fileInput.nextLine();
+            var splitLine= line.split(",");
 
+            symbols.add(new Symbol(splitLine[0]));
+        }
+        return symbols;
+    }
     public static void main(String[] args) throws FileNotFoundException {
-        Grammar g = Grammar.readFromFile("g1.txt");
-        System.out.println(g.parseInput(List.of(
-                new Symbol("a"),
-                new Symbol("+"),
-                new Symbol("a"))));
+        Grammar g = Grammar.readFromFile("g2.txt");
+
+
+        //ParseInputFile("PIF.out");
+        //ParseInputFile("seq.out");
+
+        //System.out.println(g.parseInput(ParseInputFile("seq.txt")));
+        System.out.println(g.parseInput(ParseInputFile("PIF.out")));
         /*var p = new ParsingOutput();
         p.addEntry(new ParsingOutputEntry(new Terminal("A"), -1, -1));
         p.addEntry(new ParsingOutputEntry(new Terminal("B"), 0, -1));
